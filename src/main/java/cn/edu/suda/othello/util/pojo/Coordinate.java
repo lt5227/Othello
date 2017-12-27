edd94f9c87a4b3698931ac3f91b8ea123e05a9e1
lt5227 <995362096@qq.com>
2017-12-27 13:23:09 +0800

    联机模式编写

--- src/main/java/cn/edu/suda/othello/util/pojo/Coordinate.java
+++ src/main/java/cn/edu/suda/othello/util/pojo/Coordinate.java
@@ -1,11 +1,13 @@
 package cn.edu.suda.othello.util.pojo;
 
+import java.io.Serializable;
+
 /**
  * Copyright 2017 济中节能 All rights reserved.
  * Created by LiLei on 2017/12/20 16:31.
  * 棋盘坐标对象
  */
-public class Coordinate {
+public class Coordinate implements Serializable{
     private Integer x; // X坐标
     private Integer y; // Y坐标
 
