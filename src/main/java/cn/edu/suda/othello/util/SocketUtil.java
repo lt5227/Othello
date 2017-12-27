52dfd69f38276e241627172ca67ce750a58f339b
lt5227 <995362096@qq.com>
2017-12-27 14:26:50 +0800

    联机模式编写

--- src/main/java/cn/edu/suda/othello/util/SocketUtil.java
+++ src/main/java/cn/edu/suda/othello/util/SocketUtil.java
@@ -2,6 +2,7 @@ package cn.edu.suda.othello.util;
 
 import cn.edu.suda.othello.UserBean;
 import cn.edu.suda.othello.util.pojo.Coordinate;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Component;
 
 import java.io.*;
@@ -21,12 +22,12 @@ public class SocketUtil {
      * @param coordinate 用户信息对象
      */
     public void sendUserBean(Coordinate coordinate) {
-        ObjectOutputStream oos;
         try {
-            OutputStream os = socket.getOutputStream();
-            BufferedOutputStream bos = new BufferedOutputStream(os);
-            oos = new ObjectOutputStream(bos);
-            oos.writeObject(coordinate);
+            OutputStream out = socket.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(out, "UTF-8");
+            PrintWriter pw = new PrintWriter(osw, true);
+            String message = JSONObject.toJSONString(coordinate);
+            pw.println(message);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -38,14 +39,13 @@ public class SocketUtil {
      * @return 用户信息对象
      */
     public Coordinate receiveUserBean() {
-        ObjectInputStream ois;
         try {
-            InputStream is = socket.getInputStream();
-            BufferedInputStream bis = new BufferedInputStream(is);
-            ois = new ObjectInputStream(bis);
-            Coordinate coordinate = (Coordinate) ois.readObject();
-            if (coordinate != null) {
-                return coordinate;
+            InputStream in = socket.getInputStream();
+            InputStreamReader isr = new InputStreamReader(in,"UTF-8");
+            BufferedReader br = new BufferedReader(isr);
+            String message = null;
+            while((message = br.readLine())!=null){
+                System.out.println(message);
             }
         } catch (Exception e) {
             e.printStackTrace();
